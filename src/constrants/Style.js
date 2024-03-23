729c9240ec7cff6a4f34b59eaec353cc86a4b479
StebenC <stebenc09122002@gmail.com>
2024-03-23 23:31:56 +0700

    update

---
+++ src/constrants/Style.js
@@ -0,0 +1,12 @@
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    headerImage: {
+      width: "100%",
+      height: 357,
+    },
+  });
+  
